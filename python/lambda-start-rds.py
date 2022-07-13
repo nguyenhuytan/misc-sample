6bbd75c8dc6d23806ec2c24952f81bd3ef9dc720
nguyenhuytan <nguyenhuytan@luvina.net>
2022-07-13 14:01:21 +0900

    add cluster

--- python/lambda-start-rds.py
+++ python/lambda-start-rds.py
@@ -7,6 +7,12 @@ def start_rds():
     response = client.start_db_instance(
         DBInstanceIdentifier='database-instance-id'
     )
+    
+    # start specific RDS cluster
+    response = client.start_db_cluster(
+        DBClusterIdentifier='database-cluster-id'
+    )
+    
     print(response)
 
 def lambda_handler(event, context):
